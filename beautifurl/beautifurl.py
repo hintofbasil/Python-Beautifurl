e3647ec34236da9ef9103180628920e063dab6e3
hintofbasil <hintofbasil@github.com>
2017-04-16 10:43:22 +0100

    Added overwriting of dictionary path

--- beautifurl/beautifurl.py
+++ beautifurl/beautifurl.py
@@ -2,6 +2,10 @@ import os
 
 class Beautifurl:
 
-    def __init__(self):
-        dirPath = os.path.dirname(os.path.abspath(__file__))
-        self._dictPath = os.path.join(dirPath, 'dictionaries')
+    def __init__(self,
+                dictionaryPath=None):
+        if dictionaryPath is None:
+            dirPath = os.path.dirname(os.path.abspath(__file__))
+            self._dictPath = os.path.join(dirPath, 'dictionaries')
+        else:
+            self._dictPath = os.path.abspath(dictionaryPath)
--- beautifurl/test_beautifurl.py
+++ beautifurl/test_beautifurl.py
@@ -9,3 +9,13 @@ def test_default_dictonary_path():
     print(expected)
     print(beautifurl._dictPath)
     assert beautifurl._dictPath == expected
+
+def test_overwrite_dictionary_path_abs():
+    beautifurl = Beautifurl(dictionaryPath='/fake')
+    assert beautifurl._dictPath == '/fake'
+
+def test_overwrite_dictionary_path_rel():
+    beautifurl = Beautifurl(dictionaryPath='fake')
+    expected = os.path.abspath('fake')
+    assert beautifurl._dictPath == expected
+    assert beautifurl._dictPath[0] == '/'
