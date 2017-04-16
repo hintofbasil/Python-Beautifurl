6676580a39f2e77322ea156c13d41c9566e5deb6
hintofbasil <hintofbasil@github.com>
2017-04-16 09:40:03 +0100

    Added setup file

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='beautifurl',
+    packages=['beautifurl'],
+    version='0.0.1',
+    license='MIT',  # example license
+    description='Generates beautiful urls similar to Gfycat.',
+    long_description=README,
+    url='https://github.com/hintofbasil/Python-Beautifurl',
+    author='William Hutcheson',
+    author_email='crabbybearnose@shadowmail.co.uk',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
+
