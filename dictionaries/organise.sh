ec96738625a9a3458df56be6b5d6f327d6113022
hintofbasil <hintofbasil@github.com>
2017-04-17 14:20:27 +0100

    Updated organise.sh to allow custom min length

--- dictionaries/organise.sh
+++ dictionaries/organise.sh
@@ -2,12 +2,19 @@
 
 if [ -z ${1+x} ];
 then
-	echo "USAGE: organise.sh FILE"
-	return
+	echo "USAGE: organise.sh FILE [MIN_LENGTH]"
+	exit
+fi
+
+if [ -z ${2+x} ];
+then
+	MIN_LENGTH=4
+else
+	MIN_LENGTH=$2
 fi
 
 sed -E "s/ 	/\n/g" $1 | # Replace whitespace with newlines
 	tr '[:upper:]' '[:lower:]' | # To lower case
 	uniq | # Remove duplicates
-	grep -e ".\{4,\}" | # Remove small words.  Removes excess newlines too
+	grep -e ".\{$MIN_LENGTH,\}" | # Remove small words.  Removes excess newlines too
 	sort # Sort results
