f901c2fe9b8fa6d2d2bdd8557688a0dab35f754e
hintofbasil <CuteCondemnedPenguin@shadowmail.co.uk>
2021-01-30 21:51:24 +0000

    Use pipenv for dependencies and remove min version on shuffled

--- setup.py
+++ setup.py
@@ -1,39 +1,36 @@
 import os
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+
 setup(
-    name='beautifurl',
-    packages=['beautifurl'],
-    package_data={
-        'beautifurl': ['dictionaries/*']
-    },
-    install_requires=[
-        'shuffled>=0.2'
-    ],
-    version=os.environ.get('VERSION', 'dev'),
-    license='MIT',
-    description='Generates beautiful urls similar to Gfycat.',
+    name="beautifurl",
+    packages=["beautifurl"],
+    package_data={"beautifurl": ["dictionaries/*"]},
+    version=os.environ.get("VERSION", "dev"),
+    license="MIT",
+    description="Generates beautiful urls similar to Gfycat.",
     long_description=README,
-    url='https://github.com/hintofbasil/Python-Beautifurl',
-    author='William Hutcheson',
-    author_email='crabbybearnose@shadowmail.co.uk',
+    url="https://github.com/hintofbasil/Python-Beautifurl",
+    author="William Hutcheson",
+    author_email="crabbybearnose@shadowmail.co.uk",
     test_suite="test",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: MIT License',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
     ],
+    setup_requires=["setuptools-pipfile"],
+    use_pipfile=True,
 )
-
