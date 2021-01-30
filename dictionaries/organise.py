b9ebca7e468465c355c765eb8003c1cfd1537200
hintofbasil <CuteCondemnedPenguin@shadowmail.co.uk>
2021-01-30 15:18:56 +0000

    Replace pylint with black

--- dictionaries/organise.py
+++ dictionaries/organise.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 import sys
 
+
 def main():
     if len(sys.argv) < 2:
         print("USAGE: organise.py FILE_NAME [MIN_LENGTH]")
@@ -15,17 +16,18 @@ def main():
     except (IndexError, ValueError) as ex:
         pass
 
-    with open(sys.argv[1], 'r') as f:
+    with open(sys.argv[1], "r") as f:
         read = f.readlines()
         read = [x.strip() for x in read]
-        read = filter(lambda x: ' ' not in x, read) # Filter out two word elements
-        read = filter(lambda x: len(x) >= min_length, read) # Filter out small words
-        read = filter(lambda x: '-' not in x, read)
+        read = filter(lambda x: " " not in x, read)  # Filter out two word elements
+        read = filter(lambda x: len(x) >= min_length, read)  # Filter out small words
+        read = filter(lambda x: "-" not in x, read)
         read = map(lambda x: x[0].upper() + x[1:].lower(), read)
-        read = list(set(read)) # Remove duplicates
-        read = sorted(read) # Sort
+        read = list(set(read))  # Remove duplicates
+        read = sorted(read)  # Sort
         for r in read:
             print(r)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
